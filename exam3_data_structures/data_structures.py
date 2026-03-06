25c67225e0cd85a32aa497ca451a1699eb59a542
Vakele Godongwana <vakelegawd@gmail.com>
2026-03-06 12:56:57 +0200

    Add exam3_data_structures

---
+++ exam3_data_structures/data_structures.py
@@ -0,0 +1,43 @@
+
+# ============================
+# TODO:Question 1
+# ============================
+def split_coords(coordinates: list) -> tuple:
+    return
+
+# ============================
+# TODO:Question 2
+# ============================
+def create_id_lookup(user_data: list) -> dict:
+    return
+
+# ============================
+# TODO:Question 3
+# ============================
+def extract_unique_tags(posts: list) -> set:
+    return
+
+# ============================
+# TODO:Question 4
+# ============================
+def group_by_category(items: list) -> dict:
+    return
+
+# ============================
+# TODO:Question 5
+# ============================
+def batch_api_dispatcher(user_ids: list | tuple) -> list:
+    return
+
+# ============================
+# TODO:Question 6
+# ============================
+def social_graph_inverter(following_list: dict) -> dict:
+    return
+
+
+# ============================
+# TODO:Question 7
+# ============================
+def fibonacci_generator(n: int) -> list:
+    return
