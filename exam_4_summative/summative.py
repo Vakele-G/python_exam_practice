cc162af74be4710f008eb9a9ed32c1b1a68856a4
Vakele Godongwana <vakelegawd@gmail.com>
2026-03-06 13:04:33 +0200

    Add exam_4_summative

---
+++ exam_4_summative/summative.py
@@ -0,0 +1,36 @@
+##################
+#   Question 1   #
+##################
+
+def batch_api_dispatcher(user_ids: list | tuple) -> list:
+    return
+
+##################
+#   Question 2   #
+##################
+
+def winning_streak(streak):
+    return
+
+##################
+#   Question 3   #
+##################
+
+def peak_finder(temperatures):
+    return
+
+
+##################
+#   Question 4   #
+##################
+def stage_summary(records):
+    return
+
+##################
+#   Question 5   #
+##################
+
+def draw_triangle(height):
+    return
+
+ 
